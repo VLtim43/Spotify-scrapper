4d1fea4c0cd306a0b63a915f245a7878ad28a9a9
VLtim43 <fernandoreal.valentim@gmail.com>
2023-10-05 13:44:19 -0300

    python script

--- python/generate_data.py
+++ python/generate_data.py
@@ -1,7 +1,25 @@
-# python/generate_data.py
+import json
 
-def generate_data():
-    return "Hello from Python!"
+def extract_song_data():
+    # Define the path to the input and output files
+    input_path = './data/savedsongs.json'
+    output_path = './data/orderedsongs.json'
+    
+    # Read the data from savedsongs.json
+    with open(input_path, 'r') as infile:
+        songs = json.load(infile)
+    
+    # Process the data
+    ordered_songs = []
+    for song in songs:
+        ordered_songs.append({
+            'name': song['name'],
+            'artist': song['artist']
+        })
+    
+    # Write the processed data to orderedsongs.json
+    with open(output_path, 'w') as outfile:
+        json.dump(ordered_songs, outfile, indent=4)
 
 if __name__ == "__main__":
-    print(generate_data())
+    extract_song_data()
